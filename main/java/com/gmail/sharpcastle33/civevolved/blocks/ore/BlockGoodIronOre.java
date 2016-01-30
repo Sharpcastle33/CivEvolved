58b6ab4e3a57a5080347479784af546f8c7ab029
Stephen/Sharpcastle33 <sharpcastle33@gmail.com>
2016-01-30 13:48:20 -0500

    added drops to goodIronOre

--- main/java/com/gmail/sharpcastle33/civevolved/blocks/ore/BlockGoodIronOre.java
+++ main/java/com/gmail/sharpcastle33/civevolved/blocks/ore/BlockGoodIronOre.java
@@ -1,8 +1,15 @@
 package com.gmail.sharpcastle33.civevolved.blocks.ore;
 
+import java.util.ArrayList;
+
+import com.gmail.sharpcastle33.civevolved.main.CivEvolved;
+import com.gmail.sharpcastle33.civevolved.util.Util;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class BlockGoodIronOre extends Block{
 
@@ -20,5 +27,17 @@ public class BlockGoodIronOre extends Block{
 		this.setHardness(6.0f);
 		this.setHarvestLevel("pickaxe", 1);
 	
+	}
+	
+	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune){
+		ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
+		drops.add(new ItemStack(CivEvolved.ironLargeChunk,1));
+		if(Util.chance(10)){
+			drops.add(new ItemStack(CivEvolved.ironChunk));
+		}
+		if(Util.chance(30)){
+			drops.add(new ItemStack(CivEvolved.ironSmallChunk, Util.randInt(1, 3)));
+		}
+		return drops;
 	}
 }
