1e59961bba9f686f2ba745c7d88937a71172613e
Suprakasem K <unconfigured@null.spigotmc.org>
2016-01-31 15:30:59 +0700

    Flint Knife
    Added Flint Knife item, along with FLINT crafting material.

---
+++ main/java/com/gmail/contactaquadude27/civevolved/tools/Materials.java
@@ -0,0 +1,23 @@
+package com.gmail.contactaquadude27.civevolved.tools;
+
+import com.gmail.contactaquadude27.civevolved.tools.classes.ItemFlintKnife;
+import com.gmail.contactaquadude27.civevolved.utils.RegisterHelper;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.common.util.EnumHelper;
+
+public class Materials {   
+	
+	//=======================//
+	//   FLINT TOOLS        //
+	//======================//
+	
+	static Item.ToolMaterial FLINT = EnumHelper.addToolMaterial("FLINT", 0, 60, 1F, 2.0F, 14);
+	
+	//Items
+	public static Item flintKnife = new ItemFlintKnife(FLINT).setUnlocalizedName("flintKnife");
+
+    public static void registerItems() {
+    	RegisterHelper.registerItem(flintKnife);
+    }
+}
--- main/java/com/gmail/sharpcastle33/civevolved/main/CivEvolved.java
+++ main/java/com/gmail/sharpcastle33/civevolved/main/CivEvolved.java
@@ -53,7 +53,6 @@ import net.minecraftforge.common.util.EnumHelper;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
 public class CivEvolved {
-
 	//+======+
 	//|MOD ID|
 	//+======+
@@ -64,13 +63,11 @@ public class CivEvolved {
     //|CREATIVE TABS|
     //+=============+
     public static CreativeTabs oreTab = new TabOres("oreTab");
-	
-    //+======+
+	//+======+
 	//|BLOCKS|
 	//+======+
     
     //ore
-    
     public static Block coalOre;
     public static Block copperOre;
     public static Block diamondOre;
@@ -121,8 +118,6 @@ public class CivEvolved {
     public static Item woodClub;
     //mob drops
     public static Item cowHide;
-    
-    
     //+==============+
     //|TOOL MATERIALS|
     //+==============+
