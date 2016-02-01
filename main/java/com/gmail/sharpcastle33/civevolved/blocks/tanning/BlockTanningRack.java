871e21505da87b5d5594e6500aa23137dde50f81
Stephen/Sharpcastle33 <sharpcastle33@gmail.com>
2016-01-31 22:39:18 -0500

    more tanning stuff

--- main/java/com/gmail/sharpcastle33/civevolved/blocks/tanning/BlockTanningRack.java
+++ main/java/com/gmail/sharpcastle33/civevolved/blocks/tanning/BlockTanningRack.java
@@ -1,5 +1,31 @@
 package com.gmail.sharpcastle33.civevolved.blocks.tanning;
 
-public class BlockTanningRack {
+import com.gmail.sharpcastle33.civevolved.main.CivEvolved;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class BlockTanningRack extends BlockContainer{
+	
+	public boolean isActive;
+	
+	public BlockTanningRack(){
+		super(Material.wood);
+		this.setBlockName("civevolved_tanningRackBlock");
+		this.setBlockTextureName("civevolved:tanningRackBlock");
+		this.setCreativeTab(CivEvolved.tanningTab);
+		this.setHardness(2F);
+		this.isActive = false;
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World w, int p_149915_2_) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- main/java/com/gmail/sharpcastle33/civevolved/blocks/tanning/TileTanningRack.java
+++ main/java/com/gmail/sharpcastle33/civevolved/blocks/tanning/TileTanningRack.java
@@ -1,5 +1,57 @@
 package com.gmail.sharpcastle33.civevolved.blocks.tanning;
 
-public class TileTanningRack {
+import java.util.ArrayList;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileTanningRack extends TileEntity{
+
+	ArrayList<String> allowedItems = new ArrayList<String>();
+	public ItemStack item;
+	public TileTanningRack(){
+		super();
+		allowedItems.add("cowHide");
+	}
+	
+	public void writeToNBT(NBTTagCompound tag){
+		super.writeToNBT(tag);
+		if(item != null){
+			item.writeToNBT(tag);
+		}
+	}
+	
+	public void readFromNBT(NBTTagCompound tag){
+		super.readFromNBT(tag);
+		item = ItemStack.loadItemStackFromNBT(tag);
+	}
+	
+	public ItemStack getItem(){
+		return item;
+	}
+	
+	public void setItem(ItemStack i){
+		item = i;
+	}
+	
+	public int getInventoryStackLimit(){
+		return 1;
+	}
+	
+	public boolean isUsableByPlayer(EntityPlayer p){
+		if(worldObj.getTileEntity(xCoord, yCoord, zCoord) == this 
+				&& p.getDistanceSq(xCoord + 0.5, yCoord + 0.5, zCoord + 0.5) < 64){
+			return true;
+		}else return false;
+	}
+	
+	public boolean isItemValid(ItemStack i){
+		if(allowedItems.contains(i.getItem().getUnlocalizedName())){
+			return true;
+		}else return false;
+	}
+	
+	
 }
