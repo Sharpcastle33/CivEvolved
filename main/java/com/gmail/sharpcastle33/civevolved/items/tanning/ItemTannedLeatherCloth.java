d0180f3a9297e4aa0c97cf26099642041ca34c89
Stephen/Sharpcastle33 <sharpcastle33@gmail.com>
2016-02-01 12:15:37 -0500

    cloth versions of tanned and buckskin leather

---
+++ main/java/com/gmail/sharpcastle33/civevolved/items/tanning/ItemTannedLeatherCloth.java
@@ -0,0 +1,15 @@
+package com.gmail.sharpcastle33.civevolved.items.tanning;
+
+import com.gmail.sharpcastle33.civevolved.main.CivEvolved;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemTannedLeatherCloth extends Item {
+
+	public ItemTannedLeatherCloth(){
+		this.setUnlocalizedName("civevolved_tannedLeatherCloth");
+		this.setCreativeTab(CivEvolved.tanningTab);
+		this.setTextureName("civevolved:tannedLeatherCloth");
+	}
+}
