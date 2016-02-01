4f9823e3c41aae3070478118eb551dcea3e90b94
Stephen/Sharpcastle33 <sharpcastle33@gmail.com>
2016-02-01 09:30:14 -0500

    renamed poor copper sword to poor copper club

--- main/java/com/gmail/contactaquadude27/civevolved/tools/classes/ItemPCopperClub.java
+++ main/java/com/gmail/contactaquadude27/civevolved/tools/classes/ItemPCopperClub.java
@@ -4,20 +4,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.gmail.contactaquadude27.civevolved.utils.Reference;
+import com.gmail.sharpcastle33.civevolved.items.tools.types.ItemClub;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemSword;
+import net.minecraft.item.Item.ToolMaterial;
 
-public class ItemPCopperSword extends ItemSword
+public class ItemPCopperClub extends ItemClub
 {
 	public final ToolMaterial toolMaterial;
 	static Set<Blocks> eff = new HashSet<Blocks>();
 
-	public ItemPCopperSword(ToolMaterial EnumToolMaterial)
+	public ItemPCopperClub(ToolMaterial EnumToolMaterial)
 	{
 		super(EnumToolMaterial);
 		toolMaterial = EnumToolMaterial;
